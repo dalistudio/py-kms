11d626317af354692acbb12236c70d3d1b9af861
wangdali <wangdali@qq.com>
2018-07-13 21:07:56 +0800

    添加win10和win2008r2的注册脚本

---
+++ reg-win10-1803-ent.cmd
@@ -0,0 +1,6 @@
+slmgr.vbs /upk
+slmgr.vbs /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
+slmgr.vbs /skms 39.108.173.223:1688
+slmgr.vbs /ato
+
+
---
+++ reg-win2008r2-datacenter.cmd
@@ -0,0 +1,6 @@
+slmgr.vbs /upk
+slmgr.vbs /ipk 74YFP-3QFB3-KQT8W-PMXWJ-7M648
+slmgr.vbs /skms 39.108.173.223:1688
+slmgr.vbs /ato
+
+
